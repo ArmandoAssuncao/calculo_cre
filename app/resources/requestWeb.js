c39a6659ab6dbf6d67dbf1ba193282b80d8193b8
ArmandoAssuncao <armando.assuncao.93@gmail.com>
2017-02-17 16:06:41 -0200

    updated request

--- app/resources/requestWeb.js
+++ app/resources/requestWeb.js
@@ -25,35 +25,41 @@ module.exports = {
         }, function(error, response) {
             if(error) return console.error(error);
 
-            var url = constants.url.HISTORICO_RESUMIDO;
-
             var historicHeaders = {
                 'Cache-Control': 'no-cache',
                 'Content-Type': 'application/x-www-form-urlencoded',
                 Cookie: response.headers['set-cookie'][0].split(';')[0], //get response cookie
             };
 
-            //return body html to callback
-            request.get(url, {
+            // two requests to same url to do work
+            request.get(constants.url.HISTORICO_RESUMIDO, {
                 headers: historicHeaders,
                 strictSSL: false
             }, function(error, response, body) {
-                obj = {};
-                obj.error = 1;
 
-                if(error){
-                    console.error(error);
-                }
-                //check authentication
-                else if(body.indexOf('formLogin') > -1 && body.indexOf('LOGIN') > -1){
-                    console.error('authentication error');
-                }
-                else{
-                    obj.error = 0;
-                    obj.body = body;
-                }
-                
-                callback(obj);
+                //return body html to callback
+                request.get(constants.url.HISTORICO_RESUMIDO, {
+                    headers: historicHeaders,
+                    strictSSL: false
+                }, function(error, response, body) {
+                    obj = {};
+                    obj.error = 1;
+
+                    if(error){
+                        console.error(error);
+                    }
+                    //check authentication
+                    else if(body.indexOf('formLogin') > -1 && body.indexOf('LOGIN') > -1){
+                        console.error('authentication error');
+                    }
+                    else{
+                        obj.error = 0;
+                        obj.body = body;
+                    }
+
+                    callback(obj);
+                });
+
             });
         });
     }
