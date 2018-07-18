013cee85825405eb4901740d614ac93ddfaacdf9
ArmandoAssuncao <armando.assuncao.93@gmail.com>
2018-07-17 21:36:36 -0300

    changed path name of counter_access.log

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -9,26 +9,40 @@ module.exports = function(app){
 
 // count access
 function counter_access(){
-	var PATH = '/home/logs/nodejs/calculo_cre/calculo_cre_counter_access.log';
+    var logDir = appRoot + '/logs';
+    if (!fs.existsSync(logDir)){
+        fs.mkdirSync(logDir);
+    }
+	var PATH = logDir + '/counter_access.log';
 
 	semaphore.take(function(){
-		fs.readFile(PATH, function (err, data) {
-			if (err) {
-				return console.error(err);
-			}
-			var counter = data.toString();
+        fs.access(PATH, fs.constants.F_OK, function (err) {
+            if (err) {
+                return fs.writeFile(PATH, 1, function (err) {
+                    if (err) {
+                        return console.error(err);
+                    }
+                });
+            }
 
-			counter = parseInt(counter);
-			if(isNaN(counter)){
-				counter = 0;
-			}
+            fs.readFile(PATH, function (err, data) {
+                if (err) {
+                    return console.error(err);
+                }
+                var counter = data.toString();
 
-			fs.writeFile(PATH, counter+1, function (err) {
-				if (err) {
-					return console.error(err);
-				}
-			});
-		});
+                counter = parseInt(counter);
+                if(isNaN(counter)){
+                    counter = 0;
+                }
+
+                fs.writeFile(PATH, counter+1, function (err) {
+                    if (err) {
+                        return console.error(err);
+                    }
+                });
+            });
+        });
 
 		semaphore.leave();
 	});
